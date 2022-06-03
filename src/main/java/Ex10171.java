911a16b7327905d979a55d21c82ec3140eb4de8b
GJKim <gjk0635@naver.com>
2022-06-03 09:33:07 +0900

    2022. 06. 03 Baekjoon

---
+++ src/main/java/Ex10171.java
@@ -0,0 +1,27 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Ex10171 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("\\    /\\");
+        bw.newLine();
+
+        bw.write(" )  ( ')");
+        bw.newLine();
+
+        bw.write("(  /  )");
+        bw.newLine();
+
+        bw.write(" \\(__)|");
+
+        bw.flush();
+        bw.close();
+
+    }
+
+}
