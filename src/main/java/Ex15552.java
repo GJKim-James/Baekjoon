cefeadd1bc30a2f8b8b1521d62c520d705024eab
GJKim <gjk0635@naver.com>
2022-06-19 18:33:57 +0900

    2022. 06. 19 Baekjoon

--- src/main/java/Ex15552.java
+++ src/main/java/Ex15552.java
@@ -1,17 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
-public class Ex10950 {
+public class Ex15552 {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine()); // 테스트 케이스 갯수
-
-        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -19,10 +16,13 @@ public class Ex10950 {
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
 
-            sb.append((A + B) + "\n");
+            bw.write((A + B) + "\n");
         }
 
-        System.out.println(sb);
+        br.close();
+
+        bw.flush();
+        bw.close();
 
     }
 
