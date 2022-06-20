639a7c23f84f3df11f9c15e5654aea37aab8f242
GJKim <gjk0635@naver.com>
2022-06-20 09:48:36 +0900

    2022. 06. 20 Baekjoon

---
+++ src/main/java/Ex2741.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Ex2741 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            bw.write((i + 1) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+
+}
