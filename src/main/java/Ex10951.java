54d9e5e2a3ff27c1d9bf4a080d58d98150b86c84
GJKim <gjk0635@naver.com>
2022-06-25 00:45:02 +0900

    2022. 06. 25 Baekjoon

---
+++ src/main/java/Ex10951.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Ex10951 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str;
+
+        while ((str=br.readLine()) != null) {
+
+            StringTokenizer st = new StringTokenizer(str, " ");
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            bw.write((A + B) + "\n");
+
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+
+    }
+
+}
