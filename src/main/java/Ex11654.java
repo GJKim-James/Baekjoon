37781adc7d51265f86bee8be3e3c66920115e602
GJKim <gjk0635@naver.com>
2022-07-12 20:53:29 +0900

    2022. 07. 12 Baekjoon

---
+++ src/main/java/Ex11654.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Ex11654 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int ASCII = br.read();
+
+        System.out.println(ASCII);
+
+    }
+
+}
