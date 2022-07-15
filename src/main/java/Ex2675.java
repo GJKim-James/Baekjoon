61446a63540aae12b4e7589fe7ab5b0decaf918d
GJKim <gjk0635@naver.com>
2022-07-16 01:28:15 +0900

    2022. 07. 16 Baekjoon

--- src/main/java/Ex2675.java
+++ src/main/java/Ex2675.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Ex2675 {
@@ -8,27 +6,35 @@ public class Ex2675 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine()); // 테스트 케이스
 
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < T; i++) {
 
-            String[] str = br.readLine().split(" "); // 한 줄 입력 받고 공백을 기준으로 배열에 각각 저장
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-            int R = Integer.parseInt(str[0]); // 반복 횟수 R
+            int R = Integer.parseInt(st.nextToken()); // 반복 횟수 R
 
-            String S = str[1]; // 문자열 S
+            String S = st.nextToken(); // 문자열 S
 
             for (int j = 0; j < S.length(); j++) {
                 for (int k = 0; k < R; k++) {
-                    System.out.print(S.charAt(j));
+                    sb.append(S.charAt(j));
                 }
             }
 
-            System.out.println();
+            sb.append("\n");
+
         }
 
+        bw.write(String.valueOf(sb));
+
         br.close();
+        bw.flush();
+        bw.close();
 
     }
 
