7268a4850bb901d6dde4f301d2c79e59b22487c1
GJKim <gjk0635@naver.com>
2022-08-10 21:54:19 +0900

    2022. 08. 10 Baekjoon 좌표 정렬하기

---
+++ src/main/java/정렬/Ex11650.java
@@ -0,0 +1,48 @@
+package 정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Ex11650 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine()); // 5
+
+        int[][] arr = new int[N][2];
+        // 3 4
+        // 1 1
+        // 1 -1
+        // 2 2
+        // 3 3
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (x1, x2) -> {
+            if (x1[0] == x2[0]) { // 첫 번째 원소(x 좌표)가 같다면 두 번째 원소(y 좌표)끼리 비교
+                return x1[1] - x2[1];
+            } else {
+                return x1[0] - x2[0];
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i][0] + " " + arr[i][1]).append("\n");
+        }
+
+        System.out.print(sb); // 정렬된 x 좌표 y 좌표 출력
+
+    }
+
+}
