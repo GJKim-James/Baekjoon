4d86add519befead1b45a7c2f449ed67d1afad6b
GJKim <gjk0635@naver.com>
2022-08-22 09:51:22 +0900

    2022. 08. 22 Baekjoon ??!

--- src/main/java/입출력과사칙연산/Ex10926.java
+++ src/main/java/입출력과사칙연산/Ex10926.java
@@ -1,3 +1,5 @@
+package 입출력과사칙연산;
+
 import java.io.*;
 
 public class Ex10926 {
@@ -7,7 +9,7 @@ public class Ex10926 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        bw.write(br.readLine() + "??!");
+        bw.write(br.readLine() + "??!"); // ex) joonas 입력하면 joonas??! 출력
 
         bw.flush();
         bw.close();
