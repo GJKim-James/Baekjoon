2dd285a19f6af179b3db847aabbd018890d88c43
GJKim <gjk0635@naver.com>
2022-08-23 10:03:24 +0900

    2022. 08. 23 Baekjoon 1998년생인 내가 태국에서는 2541년생?!

--- src/main/java/입출력과사칙연산/Ex18108.java
+++ src/main/java/입출력과사칙연산/Ex18108.java
@@ -1,3 +1,5 @@
+package 입출력과사칙연산;
+
 import java.io.*;
 
 public class Ex18108 {
@@ -7,9 +9,9 @@ public class Ex18108 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int y = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine()); // 불기 연도 y ex) 2541
 
-        bw.write(String.valueOf(y - 543));
+        bw.write(String.valueOf(y - 543)); // 불기 연도를 서기 연도로 변환하여 출력 ex) 1998
 
         bw.flush();
         bw.close();
