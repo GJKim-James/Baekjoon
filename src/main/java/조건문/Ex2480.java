d132e74b13901613edcc0180a054575eb3433fd1
GJKim <gjk0635@naver.com>
2022-09-04 14:08:10 +0900

    2022. 09. 04 Baekjoon 주사위 세개

--- src/main/java/조건문/Ex2480.java
+++ src/main/java/조건문/Ex2480.java
@@ -1,3 +1,5 @@
+package 조건문;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,6 +17,9 @@ public class Ex2480 {
         int b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
 
+        // 같은 눈이 3개가 나오면 10,000원+(같은 눈)×1,000원의 상금을 받게 된다.
+        // 같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금을 받게 된다.
+        // 모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금을 받게 된다.
         if (a == b & b == c) {
             System.out.println(10000 + (a * 1000));
         } else if (a == b || a == c) {
