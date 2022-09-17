5f964d000ce770780d8e26df82203c634a9e008f
GJKim <gjk0635@naver.com>
2022-09-17 18:15:14 +0900

    2022. 09. 17 Baekjoon A+B - 5

--- src/main/java/반복문/Ex10952.java
+++ src/main/java/반복문/Ex10952.java
@@ -1,3 +1,5 @@
+package 반복문;
+
 import java.io.*;
 import java.util.StringTokenizer;
 
@@ -8,12 +10,14 @@ public class Ex10952 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        // 각 테스트 케이스마다 A + B 출력
         while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
 
+            // 입력의 마지막에는 0 두 개를 써서 끝냄
             if (A == 0 && B == 0) {
                 break;
             }
