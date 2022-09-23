aae2492ceed57eaa73be5642642d6fde798c06ae
GJKim <gjk0635@naver.com>
2022-09-23 16:27:17 +0900

    2022. 09. 23 Baekjoon 평균

--- src/main/java/배열/Ex1546.java
+++ src/main/java/배열/Ex1546.java
@@ -1,3 +1,5 @@
+package 배열;
+
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
@@ -9,9 +11,9 @@ public class Ex1546 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        double[] arr = new double[Integer.parseInt(br.readLine())];
+        double[] arr = new double[Integer.parseInt(br.readLine())]; // 3
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " "); // 40 80 60
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = Double.parseDouble(st.nextToken());
@@ -19,9 +21,10 @@ public class Ex1546 {
 
         double sum = 0;
 
-        Arrays.sort(arr);
+        Arrays.sort(arr); // 40.0 60.0 80.0
 
         for (int i = 0; i < arr.length; i++) {
+            // arr[arr.length - 1]은 입력된 숫자 중 최댓값을 의미
             sum += (arr[i] / arr[arr.length - 1] * 100);
         }
 
