29b9462c7a73cb264fee363cdccd632b45efc801
GJKim <gjk0635@naver.com>
2022-09-30 10:56:05 +0900

    2022. 09. 30 Baekjoon 아스키 코드

--- src/main/java/문자열/Ex11654.java
+++ src/main/java/문자열/Ex11654.java
@@ -1,3 +1,5 @@
+package 문자열;
+
 import java.io.*;
 
 public class Ex11654 {
@@ -8,6 +10,7 @@ public class Ex11654 {
 
         int ASCII = br.read();
 
+        // 알파벳 소문자, 대문자, 숫자 0-9 중 입력으로 주어진 글자의 아스키 코드값을 출력
         System.out.println(ASCII);
 
     }
