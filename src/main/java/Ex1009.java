402b003dd37e7414328cc1c77f71ffd17baafd9a
GJKim <gjk0635@naver.com>
2022-10-14 16:43:22 +0900

    2022. 10. 14 Baekjoon 분산처리

--- src/main/java/Ex1009.java
+++ src/main/java/Ex1009.java
@@ -8,13 +8,13 @@ public class Ex1009 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int T = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine()); // 5
 
         for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken()); // 1 3 6 7 9
+            int b = Integer.parseInt(st.nextToken()); // 6 7 2 100 635
 
             int result = 1;
             for (int j = 0; j < b; j++) {
@@ -28,7 +28,7 @@ public class Ex1009 {
                 }
             }
 
-            bw.write(String.valueOf(result) + "\n");
+            bw.write(String.valueOf(result) + "\n"); // 1 7 6 1 9
         }
 
         bw.flush();
