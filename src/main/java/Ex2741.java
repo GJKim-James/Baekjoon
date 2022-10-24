1b463641347245ea90fa941028dadff28137976c
GJKim <gjk0635@naver.com>
2022-10-24 14:48:42 +0900

    2022. 10. 24 Baekjoon N 찍기

--- src/main/java/Ex2741.java
+++ src/main/java/Ex2741.java
@@ -7,10 +7,15 @@ public class Ex2741 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine()); // 5
 
         for (int i = 0; i < N; i++) {
             bw.write((i + 1) + "\n");
+            // 1
+            // 2
+            // 3
+            // 4
+            // 5
         }
 
         bw.flush();
